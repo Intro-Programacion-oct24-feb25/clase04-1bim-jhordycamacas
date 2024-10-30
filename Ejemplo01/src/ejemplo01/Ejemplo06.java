65ddb958d67d9366c7261d1024a07b6afdd858b6
jhordycamacas <jhordycamacas9@gmail.com>
2024-10-30 12:10:46 -0500

    Cambios5

--- Ejemplo01/src/ejemplo01/Ejemplo06.java
+++ Ejemplo01/src/ejemplo01/Ejemplo06.java
@@ -1,4 +1,4 @@
-/*
+                                                                                                         /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -26,7 +26,7 @@ public class Ejemplo06 {
         // usamos %f para reemplazar los valore decimales
         // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
         //        apellidoEstudiante, nacimiento);
-        System.out.printf("Nombre:%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        System.out.printf("Nombre:%s\n\nApellido:%s\n\nNacimiento:%s\n", nombreEstudiante, 
                 apellidoEstudiante, nacimiento);
         
     }
